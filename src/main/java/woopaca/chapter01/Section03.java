b34067b753a1dad6bf00cc7d36cdbde283e8fb2d
jcw1031 <jcw001031@gmail.com>
2023-07-11 16:27:47 +0900

    동작 파라미터화 연습

--- src/main/java/woopaca/chapter01/Section03.java
+++ src/main/java/woopaca/chapter01/Section03.java
@@ -2,6 +2,7 @@ package woopaca.chapter01;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Section03 {
 
@@ -84,9 +85,4 @@ class Apple {
 enum Color {
 
     RED, GREEN
-}
-
-interface Predicate<T> {
-
-    boolean test(T t);
 }
---
+++ src/main/java/woopaca/chapter02/BehaviorParameterization.java
@@ -0,0 +1,175 @@
+package woopaca.chapter02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Behavior Parameterization
+ */
+public class BehaviorParameterization {
+
+    public static void main(String[] args) {
+        List<Apple> inventory =
+                List.of(new Apple(100, Color.GREEN), new Apple(200, Color.RED), new Apple(230, Color.GREEN));
+        List<Apple> greenApples = AppleFilter.filterApples(inventory, new AppleGreenColorPredicate());
+        List<Apple> redApplesAnonymousClass =
+                AppleFilter.filterApples(inventory, new Predicate<Apple>() {
+                    @Override
+                    public boolean test(Apple apple) {
+                        return apple.getColor().equals(Color.RED);
+                    }
+                });
+        List<Apple> redApples =
+                AppleFilter.filterApples(inventory, apple -> apple.getColor().equals(Color.RED));
+        System.out.println("greenApples = " + greenApples);
+        System.out.println("redApples = " + redApples);
+
+        AppleUtils.prettyPrintApple(inventory, apple -> {
+            String weight = apple.getWeight() > 150 ? "heavy" : "light";
+            String color = apple.getColor().equals(Color.GREEN) ? "green" : "red";
+            return weight + " " + color + " apple";
+        });
+    }
+}
+
+class Apple {
+
+    private int weight;
+    private Color color;
+
+    public Apple() {
+    }
+
+    public Apple(int weight, Color color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Apple{" +
+                "weight=" + weight +
+                ", color=" + color +
+                '}';
+    }
+}
+
+enum Color {
+
+    RED, GREEN
+}
+
+class AppleFilter {
+
+    public static List<Apple> filterGreenApples(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getColor().equals(Color.GREEN)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static List<Apple> filterApplesByColor(List<Apple> inventory, Color color) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getColor().equals(color)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, Color color, int weight, boolean flag) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if ((flag && apple.getColor().equals(color) || !flag && apple.getWeight() > weight)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> predicate) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (predicate.test(apple)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+}
+
+class AppleHeavyWeightPredicate implements Predicate<Apple> {
+
+    @Override
+    public boolean test(Apple apple) {
+        return apple.getWeight() > 150;
+    }
+}
+
+class AppleGreenColorPredicate implements Predicate<Apple> {
+
+    @Override
+    public boolean test(Apple apple) {
+        return apple.getColor().equals(Color.GREEN);
+    }
+}
+
+class AppleUtils {
+
+    public static void prettyPrintApple(List<Apple> inventory, AppleFormatter appleFormatter) {
+        for (Apple apple : inventory) {
+            String output = appleFormatter.accept(apple);
+            System.out.println(output);
+        }
+    }
+}
+
+@FunctionalInterface
+interface AppleFormatter {
+
+    String accept(Apple apple);
+}
+
+class AppleFancyFormatter implements AppleFormatter {
+
+    @Override
+    public String accept(Apple apple) {
+        String weight = apple.getWeight() > 150 ? "heavy" : "light";
+        String color = apple.getColor().equals(Color.GREEN) ? "green" : "red";
+        return weight + " " + color + " apple";
+    }
+}
+
+class Filter {
+
+    public static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
+        List<T> result = new ArrayList<>();
+        for (T t : list) {
+            if (predicate.test(t)) {
+                result.add(t);
+            }
+        }
+        return result;
+    }
+}
